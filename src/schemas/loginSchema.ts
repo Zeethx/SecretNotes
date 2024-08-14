02624b9d1bbe48537c985e950987c0734e33720e
Raghav <raghavanand1869@gmail.com>
2024-08-13 23:10:40 -0400

    Added NextAuth for authentication

--- src/schemas/loginSchema.ts
+++ src/schemas/loginSchema.ts
@@ -1,6 +1,6 @@
 import {z} from 'zod';
 
 export const loginSchema = z.object({
-    credential: z.string().email().or(z.string()),
+    identifier: z.string().email().or(z.string()),
     password: z.string(),
 });
